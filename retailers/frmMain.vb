9de671df2ac283ccfeaebe596fbf50e594de7789
marloa1987 <marloajeno28@gmail.com>
2016-06-06 12:00:50 +0800

    modified

--- retailers/frmMain.vb
+++ retailers/frmMain.vb
@@ -4,12 +4,7 @@
         ' This call is required by the designer.
         InitializeComponent()
 
-        ' Add any initialization after the InitializeComponent() call.
-        'db4free
-        'database=walletretailers
-        'username=walletretailers
-        'password=gdEeireD
-        'email=marloajeno28@gmail.com 
+
     End Sub
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
         rmDatabase.Show()
